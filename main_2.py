60f845d5b30880c7a980979a27e06f166cc2aa2e
isnot-aduck <hoangduc1662002@gmail.com>
2023-06-03 10:54:27 +0700

    test main2

--- main_2.py
+++ main_2.py
@@ -1,20 +1,48 @@
-import random
+import scheduler
 import time
+import random
 import  sys
 from  Adafruit_IO import  MQTTClient
 
+count = 0
 
-AIO_USERNAME = "duchm"
-AIO_KEY = "aio_XmoJ46ilGT41mT2ZlAbIx5DDQmQr"
+IO_USERNAME =  "duchm"
+IO_KEY = "aio_hVAI68pqWK2HIfDmW7xVPFu0Qels"
 
 
-client = MQTTClient(AIO_USERNAME , AIO_KEY)
-
+## MQTT MODULE
+client = MQTTClient(IO_USERNAME , IO_KEY)
 client.connect()
 client.loop_background()
-time.sleep(5)
 
-while True:
+
+
+def Test():
+    print("Hello\n")
+def Test2():
+    global count
+    count += 1
+    print ("Task2\n")
+
+def mqtt_task_1():
     value = random.randint(0, 100)
-    client.publish("your_feed", value)
-    time.sleep(30)
+    client.publish("bbc-led", value)
+    print(value)
+
+Sche = scheduler.Scheduler()
+Sche.SCH_Init()
+
+task1=Sche.SCH_Add_Task(Test, DELAY= 0, PERIOD=1000)
+task2=Sche.SCH_Add_Task(Test2, DELAY= 0, PERIOD=1000)
+task3 = Sche.SCH_Add_Task(mqtt_task_1, DELAY=0, PERIOD=5000)
+
+
+while(1):
+    Sche.SCH_Update()
+    Sche.SCH_Dispatch_Tasks()
+    
+    if count == 5:
+        Sche.SCH_Delete(task2)
+        count += 1
+
+    time.sleep(0.1)
