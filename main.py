e700f976bad8e4494082a1bbb28c52b421478943
isnot-aduck <hoangduc1662002@gmail.com>
2023-06-03 10:45:12 +0700

    3nd commit

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ from scheduler import *
 import time                                                                                                                                                  
 from task1 import Task1                                                                                                                                      
 from task2 import Task2                                                                                                                                      
-from datetime import datetime                                                                                                                                
-                                                                                                                                                             
+from datetime import datetime         
+#pip install adafruit-io
+import random
+import  sys
+from  Adafruit_IO import  MQTTClient
+
+AIO_USERNAME = "duchm"
+AIO_KEY = "aio_XmoJ46ilGT41mT2ZlAbIx5DDQmQr"                
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.connect()
+client.loop_background()
+time.sleep(5)
+
 current_time = datetime.now().time()                                                                                                                         
 hour = current_time.strftime("%H")                                                                                                                           
 minute = current_time.strftime("%M")                                                                                                                         
@@ -22,3 +34,6 @@ while True:
     scheduler.SCH_Update()                                                                                                                                   
     scheduler.SCH_Dispatch_Tasks()                                                                                                                           
     time.sleep(0.1)   
+    value = random.randint(0, 100)
+    client.publish("your_feed", value)
+    time.sleep(30)
